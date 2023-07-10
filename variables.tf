c8784e01d9e99a49d2c661662c2513a68bc5b00c
Владислав <106807250+Destian1995@users.noreply.github.com>
2023-07-10 10:04:34 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,32 +1,32 @@
 variable "yс_token" {
-  type = string
+  type    = string
   default = "y0_AgAAAABKoqiNAATuwQAAAADNipS88UIe7R7eTAqEKnHTt3FDwpNSgPk"
 }
 
-variable "yc_folder-id" {
-  type = string
+variable "yc_folder_id" {
+  type    = string
   default = "b1ggthdvv3nparichh2u"
 }
 
 variable "yc_region" {
   default = "ru-central1-a"
-}  
+}
 
 variable "yc_cloud_id" {
   default = "ru-central1"
-}  
+}
 
 variable "network_id" {
-  type = string
+  type    = string
   default = "enpvgmdd1bchmfcvi26c"
 }
 
 variable "subnet_ids" {
-  type = string
-  default = "e9b1nkfd9dv3auhpac4e"
+  type    = list(string)
+  default = ["e9b1nkfd9dv3auhpac4e"]
 }
 
 variable "service_account_id" {
-  type = string
+  type    = string
   default = "aje2d19hpisksef93aem"
 }
